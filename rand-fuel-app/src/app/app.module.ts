dbea3f47b4e1862de210f7c530ec42870e1664a9
krzysztoffigiel <krzysztoffigiel@MacBook-Pro-Krzysztof.local>
2021-02-04 21:28:03 +0100

    login & register page styles

--- rand-fuel-app/src/app/app.module.ts
+++ rand-fuel-app/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { RegisterComponent } from './components/register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +21,10 @@ import { AngularMaterialModule } from './angular-material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    FlexLayoutModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
